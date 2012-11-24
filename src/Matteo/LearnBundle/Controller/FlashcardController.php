b317d103b99e2b3a58669d53d3cb595b93bc7c96
Matteo De Micheli <mat.demicheli@gmail.com>
2012-11-24 18:05:45 +0100

    Added functionality to add one flashcards to specified card boxes (through the show view of the card box)

--- src/Matteo/LearnBundle/Controller/FlashcardController.php
+++ src/Matteo/LearnBundle/Controller/FlashcardController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Matteo\LearnBundle\Entity\Flashcard;
 use Matteo\LearnBundle\Form\FlashcardType;
+use Matteo\LearnBundle\Entity\Cardbox;
 
 /**
  * Flashcard controller.
@@ -15,38 +16,55 @@ use Matteo\LearnBundle\Form\FlashcardType;
 class FlashcardController extends Controller
 {
     /**
-     * For adding new Flashcards to a cardbox.
+     * Create Form adding or editing existing Flashcards to a specified cardbox.
      *
      */
-     
-    /*
-    public function addAction(Request $request, $id)
+    public function addOrEditAction(Request $request, $id)
     {
+        $em = $this->getDoctrine()->getManager();
+        $currentCardbox = $em->getRepository('MatteoLearnBundle:Cardbox')->find($id);
+        
         $entity = new Flashcard();
-        $cardboxForm = $this->createForm(new FlashcardType(), $entity);
+        $form = $this->createForm(new FlashcardType(), $entity);
         
-        $entity = $em->getRepository('MatteoLearnBundle:FlashcardRepository')->();
+
+        // $entity = $em->getRepository('MatteoLearnBundle:FlashcardRepository')->();
         
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find the Category entity.');
-        }
+        return $this->render('MatteoLearnBundle:Flashcard:addCards.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+            'cardbox'=> $currentCardbox,
+        ));
+    }
+    
+    /**
+     * Create Action for a specified cardbox.
+     *
+     */
+    public function createAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $currentCardbox = $em->getRepository('MatteoLearnBundle:Cardbox')->find($id);
         
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createForm(new CategoryType(), $entity);
-        $editForm->bind($request);
+        // $entity = $em->getRepository('MatteoLearnBundle:FlashcardRepository')->findFlashcardsWithCardbox($id);
         
-        if ($editForm->isValid()) {
+        $entity = new Flashcard();
+        $entity->setCardbox($currentCardbox);
+        $form = $this->createForm(new FlashcardType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('learn_category_edit', array('id' => $id)));
+            return $this->redirect($this->generateUrl('learn_cardbox_show', array('id' => $currentCardbox->getId())));
         }
-        
-        return $this->render('MatteoLearnBundle:Category:addCards.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+
+        return $this->render('MatteoLearnBundle:Flashcard:addCards.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+            'cardbox'=> $currentCardbox,
         ));
     }
-    */
 }
